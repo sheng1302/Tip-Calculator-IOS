0dd37863459ecc3dc4941ab9fbfb39fd61433f60
Sheng Liu <sheng1302@gmail.com>
2019-01-12 12:01:52 -0500

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tip-Calculator-IOS
+**Tip Calculator** is an iOS tip calculator that helps to split bills with your friends.
+
+Purpose: pre-work submission for CodePath application.
+
--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalCostLabel: UILabel!
+    @IBOutlet weak var individualCostLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var costField: UITextField!
+    @IBOutlet weak var amountPeopleField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func calculateTotalCost(_ sender: Any) {
 
+        let totalCost = calculateTotalCostHelper()
+        totalCostLabel.text = String(format: "$%.2f", totalCost)
+        
+        let finalTip = calculateIndividualCostHelper()
+        individualCostLabel.text = String(format: "$%.2f", (finalTip))
+    
+    }
+    
+    
+    func calculateIndividualCostHelper() -> Double {
+        
+        let people = Double(amountPeopleField.text!) ?? 1
+        let individualCost = Double( calculateTotalCostHelper() / people)
+        
+        return individualCost
+    }
+    
+    func calculateTotalCostHelper() -> Double{
+        let bill = Double(costField.text!) ?? 0
+        let tip = calculateTip()
+        let totalCost = bill + tip
+        
+        return totalCost
+    }
+    
+    func calculateTip() -> Double{
+        let bill = Double(costField.text!) ?? 0
+        let tipPercentage = [0.10, 0.15, 0.20, 0.25]
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        
+        return tip
+    }
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true);
+    }
+    
 }
 
